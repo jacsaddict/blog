25144a1f06313684f784d4106cd9555281ed5a72
張育榮 <j1997860221@gmail.com>
2019-04-24 14:39:57 +0900

    [Shell] associate to source files

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,17 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# Source maintaining
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
